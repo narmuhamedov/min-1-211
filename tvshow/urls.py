85e5dc0112a8613cc7778a87b451f8f8b83c21eb
rz <kingradomir@gmail.com>
2023-03-06 10:20:44 +0600

    'class'

--- tvshow/urls.py
+++ tvshow/urls.py
@@ -2,10 +2,18 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('tvshow/', views.tvshow_view, name='show'),
-    path('tvshow/<int:id>/', views.tvshow_detail_view, name='show_detail'),
-    path('tvshow/<int:id>/update/', views.update_tv_show, name='update'),
-    path('tvshow/<int:id>/delete/',
-         views.delete_tv_show, name='delete'),
-    path('add-show/', views.add_tv_show_view, name='add'),
+    #path('tvshow/', views.tvshow_view, name='show'),
+    path('tvshow/', views.TvShowView.as_view(), name='show'),
+
+    #path('tvshow/<int:id>/', views.tvshow_detail_view, name='show_detail'),
+    path('tvshow/<int:id>/', views.TvShowDetailView.as_view(), name='show_detail'),
+    #path('tvshow/<int:id>/update/', views.update_tv_show, name='update'),
+    path('tvshow/<int:id>/update/', views.TvShowUpdateView.as_view(), name='update'),
+
+    # path('tvshow/<int:id>/delete/',
+    #      views.delete_tv_show, name='delete'),
+    path('tvshow/<int:id>/delete/', views.TvShowDeleteView.as_view(), name='delete'),
+    #path('add-show/', views.add_tv_show_view, name='add'),
+    path('add-show/', views.TvShowCreateView.as_view(), name='add'),
+
 ]
--- tvshow/views.py
+++ tvshow/views.py
@@ -1,59 +1,105 @@
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from . import models, forms
+from django.views import generic
 
 #не полная информация
-def tvshow_view(requests):
-    show = models.TvShow.objects.all()
-    return render(requests, 'tvshow.html', {'show': show})
+
+class TvShowView(generic.ListView):
+    template_name = 'tvshow.html'
+    queryset = models.TvShow.objects.all
+
+    def get_queryset(self):
+        return models.TvShow.objects.all()
+
+# def tvshow_view(requests):
+#     show = models.TvShow.objects.all()
+#     return render(requests, 'tvshow.html', {'show': show})
 
 
 #детальная информация
-def tvshow_detail_view(request, id):
-    show_id = get_object_or_404(models.TvShow, id=id)
-    return render(request, 'tvshow_detail.html', {'show_id': show_id})
+class TvShowDetailView(generic.DetailView):
+    template_name = 'tvshow_detail.html'
+
+    def get_object(self, **kwargs):
+        show_id = self.kwargs.get('id')
+        return get_object_or_404(models.TvShow, id=show_id)
+
+# def tvshow_detail_view(request, id):
+#     show_id = get_object_or_404(models.TvShow, id=id)
+#     return render(request, 'tvshow_detail.html', {'show_id': show_id})
 
 
 #добавление шоу
-def add_tv_show_view(request):
-    method = request.method
-    if method == 'POST':
-        form = forms.TvShowForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return HttpResponse('Фильм Успешно добавлен')
+class TvShowCreateView(generic.CreateView):
+    template_name = 'add_tv_show.html'
+    form_class = forms.TvShowForm
+    queryset = models.TvShow.objects.all()
+    success_url = '/tvshow/'
 
-    else:
-        form = forms.TvShowForm()
+    def form_valid(self, form):
+        print(form.cleaned_data)
+        return super(TvShowCreateView, self).form_valid(form=form)
 
-    return render(request, 'add_tv_show.html', {'form': form})
+# def add_tv_show_view(request):
+#     method = request.method
+#     if method == 'POST':
+#         form = forms.TvShowForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             form.save()
+#             return HttpResponse('Фильм Успешно добавлен')
+#
+#     else:
+#         form = forms.TvShowForm()
+#
+#     return render(request, 'add_tv_show.html', {'form': form})
 
 #Изменение шоу
-def update_tv_show(request, id):
-    show_object = get_object_or_404(models.TvShow, id=id)
-    if request.method == 'POST':
-        form = forms.TvShowForm(instance=show_object, data=request.POST)
-        if form.is_valid():
-            form.save()
-            return HttpResponse('Успешно обновлено!!!')
-    else:
-        form = forms.TvShowForm(instance=show_object)
+class TvShowUpdateView(generic.UpdateView):
+    template_name = 'update_tv.html'
+    form_class = forms.TvShowForm
+    success_url = '/tvshow/'
 
-    return render(request, 'update_tv.html',
-                  {
-                      'form': form,
-                      'object': show_object
-                  }
-                  )
+    def get_object(self, **kwargs):
+        show_id = self.kwargs.get('id')
+        return get_object_or_404(models.TvShow, id=show_id)
 
+    def form_valid(self, form):
+        return super(TvShowUpdateView, self).form_valid(form=form)
+
+
+# def update_tv_show(request, id):
+#     show_object = get_object_or_404(models.TvShow, id=id)
+#     if request.method == 'POST':
+#         form = forms.TvShowForm(instance=show_object, data=request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return HttpResponse('Успешно обновлено!!!')
+#     else:
+#         form = forms.TvShowForm(instance=show_object)
+#
+#     return render(request, 'update_tv.html',
+#                   {
+#                       'form': form,
+#                       'object': show_object
+#                   }
+#                   )
+#
 
 #Удаление
-def delete_tv_show(request,id):
-    show_object = get_object_or_404(models.TvShow,
-                                    id=id)
-    show_object.delete()
-    return HttpResponse('Успешно удален!!!')
+class TvShowDeleteView(generic.DeleteView):
+    template_name = 'confirm_delete.html'
+    success_url = "/tvshow/"
 
+    def get_object(self, **kwargs):
+        show_id = self.kwargs.get('id')
+        return get_object_or_404(models.TvShow, id=show_id)
+# def delete_tv_show(request,id):
+#     show_object = get_object_or_404(models.TvShow,
+#                                     id=id)
+#     show_object.delete()
+#     return HttpResponse('Успешно удален!!!')
+#
 
 
 
